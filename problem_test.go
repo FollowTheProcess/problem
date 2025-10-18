279479750b4d3119392acc8add2269817f38f19f
Tom Fleet <me@followtheprocess.codes>
2025-10-18 11:18:29 +0100

    Implement a problem HTTP response helper

--- problem_test.go
+++ problem_test.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json/jsontext"
 	"encoding/json/v2"
 	"flag"
+	"io"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"path/filepath"
 	"testing"
@@ -115,6 +117,52 @@ func TestProblemJSON(t *testing.T) {
 			test.Ok(t, err)
 
 			test.DiffBytes(t, got, want)
+		})
+	}
+}
+
+func TestRespond(t *testing.T) {
+	tests := []struct {
+		name    string           // Name of the test case
+		body    string           // Expected JSON body
+		options []problem.Option // Options to pass to Respond
+		status  int              // Expected HTTP status on the response
+	}{
+		{
+			name:    "empty",
+			options: []problem.Option{},
+			status:  http.StatusOK, // If w.WriteHeader isn't called explicitly, it defaults to 200
+			body:    `{"type":"about:blank"}`,
+		},
+		{
+			name: "teapot",
+			options: []problem.Option{
+				problem.Status(http.StatusTeapot),
+			},
+			status: http.StatusTeapot,
+			body:   `{"type":"about:blank","status":418}`,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handler := func(w http.ResponseWriter, _ *http.Request) {
+				problem.Respond(w, tt.options...)
+			}
+
+			req := httptest.NewRequest(http.MethodGet, "/", nil)
+			w := httptest.NewRecorder()
+			handler(w, req)
+
+			res := w.Result()
+			defer res.Body.Close()
+
+			test.Equal(t, res.StatusCode, tt.status)
+
+			body, err := io.ReadAll(res.Body)
+			test.Ok(t, err)
+
+			test.DiffBytes(t, body, []byte(tt.body))
 		})
 	}
 }
